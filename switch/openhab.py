655c7522a88d912535efdd97d8973fa02f3e6767
Brenda Wallace <brenda@wallace.net.nz>
2017-01-08 19:39:55 +1300

    Updating comments

--- switch/openhab.py
+++ switch/openhab.py
@@ -8,10 +8,11 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the sensor platform."""
+    """Setup the openhab switch platform."""
     devices = []
     response = requests.get('{host}/rest/items?type=json'.format(
         host=config.get('host')))
+    # Add all SwitchItem entries to homeassistant
     for item in response.json()['item']:
         if item['type'] in ['SwitchItem', ]:
             devices.append(OpenhabSwitch(item))
@@ -26,13 +27,13 @@ class OpenhabSwitch(SwitchDevice):
 
     @property
     def name(self):
-        """Return the name of the sensor."""
+        """Return the name of the openhab item."""
         return self._item['name']
 
     @property
     def is_on(self):
-        """Return true if light is on."""
         _LOGGER.info("Reading cached item state: {}".format(self._item))
+        """Return true if switch is on."""
         return self._item['state'] == 'ON'
 
     def turn_on(self, **kwargs):
